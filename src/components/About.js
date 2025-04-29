1514becb4ed5e03b1b6bc736926bcabe0cd3eaa1
Emmanuel Ngetich <engetich333@gmail.com>
2025-04-28 22:17:47 -0700

    complete props implementation and fix conditional rendering

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,26 @@
 import React from "react";
+import Links from "./Links";
+import PropTypes from 'prop-types';
 
-function About() {
+function About({bio, links = {}}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      {bio && bio.trim() !== "" && <p>{bio}</p>}
+      <Links {...links} />
     </div>
   );
 }
+
+About.propTypes = {
+  bio: PropTypes.string,
+  links: PropTypes.shape({
+    github: PropTypes.string.isRequired,
+    linkedin: PropTypes.string.isRequired
+  }).isRequired
+};
+
 
 export default About;
